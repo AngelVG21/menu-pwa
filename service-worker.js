1a2da8fa8f50ca9decb2a507bebfbcba1ab220ac
AngelVG21 <147109435+AngelVG21@users.noreply.github.com>
2025-12-25 17:40:05 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,22 @@
 const CACHE_NAME = "menu-pwa-v1"
 const ARCHIVOS_CACHE = [
     "./",
-    "./index.html",
-    "./styles.css",
-    "./menu.js",
-    "./manifest.json"
+    "index.html",
+    "styles.css",
+    "menu.js",
+    "manifest.json"
 ]
 self.addEventListener("install", event => {
     event.waitUntil(caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(ARCHIVOS_CACHE)
-        })  
-    )
+            cache.addAll(FILES))
+            )
+        self.skipWaiting()
+    })
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim())
 })
 self.addEventListener("fetch", event => {
-    event.respondWith(caches.match(event.request).then(respuesta => {
-            return respuesta || fetch(event.request)
-        })
+    event.respondWith(caches.match(event.request).then(response => {
+         response || fetch(event.request))
     )
 })
